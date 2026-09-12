011af42d8cfb76bbbc37eb750cdd9ae4aa6232c4
ABDUL AHAD LODHI <alodhi2020@gmail.com>
2026-09-12 11:41:17 -0700

    Delete task8_c.cpp

--- task8_c.cpp
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-int main(){
-	char name[30];
-	printf("Enter your full name ");
-	scanf("%s",&name);
-	printf("Hello %s Welcome to the world of fast where you learn many important things",name);
-}
